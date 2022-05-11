5d7e111bbc6a5451cbf1d29831b5512cd19de901
Phillip2608 <luizlindo2416@gmail.com>
2022-05-11 15:11:36 -0300

    starting the project dropbox Clone

---
+++ app/public/src/controller/DropBoxController.js
@@ -0,0 +1,25 @@
+class DropBoxController{
+    constructor(){
+        this.btnSendFileEl = document.querySelector('#btn-send-file');
+        this.inputFilesEl = document.querySelector('#files');
+        this.snackModalEl = document.querySelector('#react-snackbar-root'); 
+
+        this.initEvents();
+    }
+
+    initEvents(){
+        this.btnSendFileEl.addEventListener('click', e=>{
+            this.inputFilesEl.click();
+        });
+
+        this.inputFilesEl.addEventListener('change', e=>{
+            console.log(e.target.files);
+
+            this.snackModalEl.style.display = "block";
+        });
+    }
+
+    uploadTask(){
+        
+    }
+}
